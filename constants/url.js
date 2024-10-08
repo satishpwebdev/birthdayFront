0599597237d0bfa764a26966ff05eb116b0ee6a9
Satish Prajapati <72158548+satishpwebdev@users.noreply.github.com>
2024-10-09 01:30:40 +0530

    Updating the url.js

--- constants/url.js
+++ constants/url.js
@@ -1,1 +1,2 @@
-export const liveUrl = "https://birth-app-back.vercel.app"
+//export const liveUrl = "https://birth-app-back.vercel.app"
+export const liveUrl = "https://birthappback-production.up.railway.app"
