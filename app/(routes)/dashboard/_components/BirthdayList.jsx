8c31f63d43878c5a0ec451531e7e3079b7c4b351
Satish Prajapati <72158548+satishpwebdev@users.noreply.github.com>
2025-03-21 18:45:47 +0530

    Update BirthdayList.jsx

--- app/(routes)/dashboard/_components/BirthdayList.jsx
+++ app/(routes)/dashboard/_components/BirthdayList.jsx
@@ -7,6 +7,7 @@ import RibbonTwo from "../../../../public/celefloat2.png";
 import Image from "next/image";
 import axios from "axios";
 import { liveUrl } from "@/constants/url";
+import { Typewriter } from "./Effect";
 
 const BirthdayList = () => {
   const [birthdays, setBirthdays] = useState([]);
@@ -16,9 +17,9 @@ const BirthdayList = () => {
 
   const getDataFromApi = async () => {
     try {
-     const res = await axios.get(`${liveUrl}/tasks`, {
-      headers: { 'x-api-key': process.env.NEXT_PUBLIC_API_KEY } 
-    });
+      const res = await axios.get(`${liveUrl}/tasks`, {
+        headers: { "x-api-key": process.env.NEXT_PUBLIC_API_KEY }
+      });
       const apiRes = await res.data;
       setBirthdays(apiRes);
     } catch (error) {
@@ -39,11 +40,9 @@ const BirthdayList = () => {
       day: "2-digit"
     });
 
-    // Today's Birthdays
-
     const todays = birthdays.filter((birthday) => {
-      const birthDateString = `${birthday.birthDate} ${today.getFullYear()}`; 
-      const birthDate = new Date(birthDateString); 
+      const birthDateString = `${birthday.birthDate} ${today.getFullYear()}`;
+      const birthDate = new Date(birthDateString);
       return (
         birthDate.toLocaleDateString("en-US", {
           month: "2-digit",
@@ -52,46 +51,50 @@ const BirthdayList = () => {
       );
     });
 
-    // Current Month Birthdays
-
     const currentMonthList = birthdays.filter((birthday) => {
-      const birthDateString = `${birthday.birthDate} ${today.getFullYear()}`; 
+      const birthDateString = `${birthday.birthDate} ${today.getFullYear()}`;
       const birthDate = new Date(birthDateString);
-      return birthDate.getMonth() === currentMonth && birthDate >= today; 
+      return birthDate.getMonth() === currentMonth && birthDate >= today;
     });
 
-    // Next Month Birthdays
     const nextMonthList = birthdays.filter((birthday) => {
       const birthDateString = `${birthday.birthDate} ${today.getFullYear()}`;
       const birthDate = new Date(birthDateString);
-      return birthDate.getMonth() === nextMonth; // Check if birthday is in the next month
+      return birthDate.getMonth() === nextMonth;
     });
 
-    // Set the state with filtered birthdays
     setTodaysBirthdays(todays);
     setCurrentMonthBirthdays(currentMonthList);
     setNextMonthBirthdays(nextMonthList);
-
   }, [birthdays]);
 
-
-  //Formatting Date to show the users 
   const formatDate = (birthDate) => {
-    const [month, day] = birthDate.split(" "); // Split "10 04" into ["10", "04"]
-    
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const monthIndex = parseInt(month, 10) - 1; // Convert "10" to 9 for indexing months
-  
-    // Convert day to integer for further formatting
+    const [month, day] = birthDate.split(" ");
+
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    const monthIndex = parseInt(month, 10) - 1;
+
     const dayOfMonth = parseInt(day, 10);
-  
-    // Adding ordinal suffix (st, nd, rd, th)
+
     let suffix = "th";
     if (dayOfMonth % 10 === 1 && dayOfMonth !== 11) suffix = "st";
     else if (dayOfMonth % 10 === 2 && dayOfMonth !== 12) suffix = "nd";
     else if (dayOfMonth % 10 === 3 && dayOfMonth !== 13) suffix = "rd";
-  
-    return `${dayOfMonth}${suffix} ${monthNames[monthIndex]}`; // "4th October"
+
+    return `${dayOfMonth}${suffix} ${monthNames[monthIndex]}`;
   };
 
   return (
@@ -110,67 +113,83 @@ const BirthdayList = () => {
       <div className="hidden md:block ribbon absolute top-10 right-10">
         <Image width={160} height={150} src={RibbonTwo} alt="Ribbon Two" />
       </div>
+
       <div className="relative pt-10 sm:pt-18 lg:w-full mx-auto min-h-screen">
         <h1 className="text-gray-900 dark:text-white font-bold text-4xl sm:text-5xl md:text-6xl xl:text-7xl text-center">
-          Birthday Reminders
+          <span className="bg-gradient-to-r from-rose-500 to-amber-400 bg-clip-text text-transparent">
+            Birthday
+          </span>{" "}
+          Reminders
         </h1>
+
         <p className="mt-4 sm:mt-8 text-gray-700 dark:text-gray-300 text-base sm:text-lg md:text-xl text-center">
-          Stay on top of special days with your birthday tracker!
+          <Typewriter
+            texts={["Have a great birthday !", "Cheers to your day !"]}
+            colors={[
+              "bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent",
+              "bg-gradient-to-t from-red-500 to-yellow-500 bg-clip-text text-transparent",
+              "text-red-500"
+            ]}
+            speed={100}
+            eraseSpeed={70}
+            delay={2000}
+            loop={true}
+          ></Typewriter>
         </p>
 
-        <div className=" relative mt-8 sm:mt-16 grid grid-cols-1 gap-8 sm:grid-cols-3 ">
+        <div className="flex flex-col sm:flex-row gap-8 mt-8 sm:mt-16 justify-center">
           {/* Today's Birthdays */}
-          <div className="p-6 sm:p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg">
-            <h2 className="text-xl sm:text-md font-semibold text-gray-900 dark:text-white mb-4">
-              Today's Birthdays
-            </h2>
-            <ul>
-              {todaysBirthdays.length > 0 ? (
-                todaysBirthdays.map((birthday, index) => (
-                  <li key={index} className="text-base sm:text-sm text-gray-700 dark:text-gray-300 mb-2">
-                    🎉 {birthday.firstName} - {formatDate(birthday.birthDate)}
-                  </li>
-                ))
-              ) : (
-                <li className="text-gray-500 dark:text-gray-400">No birthdays today.</li>
-              )}
-            </ul>
+          <div className="p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg w-full sm:w-1/3 h-52">
+            <h2 className="text-xl font-semibold text-gray-900 dark:text-white mb-4">Today's Birthdays</h2>
+            <div className="h-32 overflow-y-auto scrollbar ">
+              <ul>
+                {todaysBirthdays.length > 0 ? (
+                  todaysBirthdays.map((birthday, index) => (
+                    <li key={index} className="text-base text-gray-700 dark:text-gray-300 mb-2">
+                      🎉 {birthday.firstName} - {formatDate(birthday.birthDate)}
+                    </li>
+                  ))
+                ) : (
+                  <li className="text-gray-500 dark:text-gray-400">No birthdays today.</li>
+                )}
+              </ul>
+            </div>
           </div>
 
-          {/* Current Month Birthdays */}
-          <div className="p-6 sm:p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg ">
-            <h2 className="text-xl sm:text-md font-semibold text-gray-900 dark:text-white mb-4">
-              Upcoming Birthdays
-            </h2>
-            <ul>
-              {currentMonthBirthdays.length > 0 ? (
-                currentMonthBirthdays.map((birthday, index) => (
-                  <li key={index} className="text-base sm:text-sm text-gray-700 dark:text-gray-300 mb-2">
-                    🎂 {birthday.firstName} {birthday.lastName} - {formatDate(birthday.birthDate)}
-                  </li>
-                ))
-              ) : (
-                <li className="text-gray-500 dark:text-gray-400">No more birthdays this month.</li>
-              )}
-            </ul>
+          {/* Upcoming Birthdays */}
+          <div className="p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg w-full sm:w-1/3 h-52">
+            <h2 className="text-xl font-semibold text-gray-900 dark:text-white mb-4">Upcoming Birthdays</h2>
+            <div className="h-32 overflow-y-auto scrollbar">
+              <ul>
+                {currentMonthBirthdays.length > 0 ? (
+                  currentMonthBirthdays.map((birthday, index) => (
+                    <li key={index} className="text-base text-gray-700 dark:text-gray-300 mb-2">
+                      🎂 {birthday.firstName} {birthday.lastName} - {formatDate(birthday.birthDate)}
+                    </li>
+                  ))
+                ) : (
+                  <li className="text-gray-500 dark:text-gray-400">No more birthdays this month.</li>
+                )}
+              </ul>
+            </div>
           </div>
 
           {/* Next Month Birthdays */}
-          <div className="p-6 sm:p-8 bg-white dark:bg-gray-800 rounded-lg shadow-lg">
-            <h2 className="text-xl sm:text-md font-semibold text-gray-900 dark:text-white mb-4">
-              Next Month Birthdays
-            </h2>
-            <ul>
-              {nextMonthBirthdays.length > 0 ? (
-                nextMonthBirthdays.map((birthday, index) => (
-                  <li key={index} className="text-base sm:text-sm text-gray-700 dark:text-gray-300 mb-2">
-                    🎂 {birthday.firstName} {birthday.lastName} - {formatDate(birthday.birthDate)}
-                  </li>
-                ))
-              ) : (
-                <li className="text-gray-500 dark:text-gray-400">No birthdays next month.</li>
-              )}
-            </ul>
+          <div className="p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg w-full sm:w-1/3 h-52">
+            <h2 className="text-xl font-semibold text-gray-900 dark:text-white mb-4">Next Month Birthdays</h2>
+            <div className="h-32 overflow-y-auto scrollbar">
+              <ul>
+                {nextMonthBirthdays.length > 0 ? (
+                  nextMonthBirthdays.map((birthday, index) => (
+                    <li key={index} className="text-base text-gray-700 dark:text-gray-300 mb-2">
+                      🎂 {birthday.firstName} {birthday.lastName} - {formatDate(birthday.birthDate)}
+                    </li>
+                  ))
+                ) : (
+                  <li className="text-gray-500 dark:text-gray-400">No birthdays next month.</li>
+                )}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
